1c9ec921dd00d092ff7b031102a073cd0eaa833a
Shravan Dwarka <shravandwarka97@gmail.com>
2025-01-30 21:33:10 -0500

    Learn Go-Mod
    - Using Hellos function
    - Passing a slice of names
    - Added for loop to print individual values

--- hello.go
+++ hello.go
@@ -11,11 +11,15 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Shravan")
+	names := []string{"Shravan", "Sudhveer", "Rachna"}
+
+	message, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	for _, msg := range message {
+		fmt.Println(msg)
+	}
 }
