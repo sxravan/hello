873fb99fc5b136d8aee363ba000237d0ddb33e5c
Shravan Dwarka <shravandwarka97@gmail.com>
2025-01-30 21:06:44 -0500

    Added logs

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/sxravan/hello
 
 go 1.23.5
 
-require github.com/sxravan/greetings v0.0.0-20250131011147-3abd35d4b9d9
+require github.com/sxravan/greetings v1.0.2
--- hello.go
+++ hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/sxravan/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Shravan")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Shravan")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
